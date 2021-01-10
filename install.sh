3e49c47167e59bca767a0386b72baeb5c70caaed
BLACK-KILLER <63947251+ShuBhamg0sain@users.noreply.github.com>
2021-01-10 12:04:27 +0530

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+echo $red 
+echo ""----------------------------------------------------------------------------- ""
+echo -e "\e[1;31m  This Script is ShuBhamg0sain by now start"
+echo "---------------------------------------------------------------------------------"
+
+echo "007"
+echo "007"
+echo "007"
+echo "."
+
+echo -e "\e[1;32m   #####################################"
+echo -e "\e[1;32m    we are anynomous"               
+echo -e "\e[1;32m    we are hackers ok "                                                                    
+echo -e "\e[1;32m   ####################################"
+
+echo "007"
+echo "007"
+echo "007"
+
+echo "."
+ls
+cd
+cd
+git clone https://github.com/ShuBhamg0sain/adb-server
+cd adb-server
+chmod +x install-adb-server.sh
+bash install-adb-server.sh
+ls
+cd
+git clone https://github.com/ShuBhamg0sain/6digit-phone-pin
+cd 6digit-phone-pin
+chmod +x 6digipincracker
+bash 6digipincracker
+
+echo -e "\e[1;32m  #####################################"
+echo -e "\e[1;32m   u can now hack anything      "               
+echo -e "\e[1;32m    completing wait 3 2 1 "                                                                    
+echo -e "\e[1;32m  ####################################"
+
+
+echo -e "\e[1;31m  #####################################"
+echo -e "\e[1;31m  BLACK-KILLER "               
+echo -e "\e[1;31m    join us on facebook https://m.facebook.com/shubham.gosain.980 "                                                                   
+echo -e "\e[1;31m  ####################################"
+
+
+echo " "                                                                                        
+echo -e "\e[1;31m  #####################################"
+echo -e "\e[1;31m   https://www.instagram.com/shubham_g0sain/?hl=en "                                                                               
+echo -e "\e[1;31m   ####################################"
+
+
+echo -e "\e[1;34m  #####################################"              
+echo -e "\e[1;34m    processing wait......"                                                                    
+echo -e "\e[1;34m  ####################################"
+
+echo " If You Connected To Internet Then u downloaded success fully follow me Instagram  "
+
+echo " ??? now rock and hack ??? "
+
+exit
